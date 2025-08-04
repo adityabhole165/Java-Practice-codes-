dd20205becc939fd0af265b0c165a57d4a30e34b
Aditya Bhole <adityabhole0304@gmail.com>
2025-08-04 12:32:23 +0530

    leap year

---
+++ leapYear.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class leapYear{
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        
+        int year = sc.nextInt();
+        
+        boolean x = (year % 4 == 0);
+        boolean y = (year % 100 != 0);
+        boolean z =(year % 400 == 0) && (
+            year % 100 == 0
+        );
+
+        if( x && (y ||z)) {
+            System.out.println(year + "  is leap year");
+        } else {
+            System.out.print("not a leap year");
+        }
+    }
+}
