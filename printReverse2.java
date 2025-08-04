914b433222ab1269f72f19e928201ce3e3de8f64
Aditya Bhole <adityabhole0304@gmail.com>
2025-08-04 13:04:28 +0530

    Reverse number approach 2

---
+++ printReverse2.java
@@ -0,0 +1,13 @@
+public class printReverse2 {
+    public static void main(String args[]) {
+        int number = 54687;
+        int rev = 0;
+        
+        while(number  > 0) {
+            int last  = number % 10;
+            rev = (rev * 10) + last;
+            number /= 10;
+        }
+        System.out.print(rev);
+    }
+}
