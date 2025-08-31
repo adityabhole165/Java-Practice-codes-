ea84df1c2689b9fcab68d7b639563a24ffbbc0cf
Aditya Bhole <adityabhole0304@gmail.com>
2025-08-31 16:24:09 +0530

    for loop concept

---
+++ Strivers_Sheet/loop1.java
@@ -0,0 +1,7 @@
+public class loop1{
+    public static void main(String args[]){
+        for(int i =1; i <=10 ; i++) {
+            System.out.println("Hey , Aditya , this is the " + i + " 'th iteration ");
+        }
+    }
+}
