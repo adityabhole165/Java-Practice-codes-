116a4e4c3e1071ac76f1da89f37b2afcc6367cb3
Aditya Bhole <adityabhole0304@gmail.com>
2025-08-31 16:32:58 +0530

    Conditiopnal statements inside for loop

---
+++ Strivers_Sheet/CondinINfor.java
@@ -0,0 +1,13 @@
+public class CondinINfor {
+    public static void main(String args[]) {
+        for(int i =1; i<=10; i++){
+            if( i %2 == 0){
+                System.out.println("Even Number= " +  i);
+            
+            }else {
+                System.out.println("Odd number = "+ i);
+            
+            }
+        }
+    }
+}
