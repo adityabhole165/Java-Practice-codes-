19d05834732a6017958478e63cc7dab9f795359e
Aditya Bhole <adityabhole0304@gmail.com>
2025-09-02 15:25:47 +0530

    Do while Loop

---
+++ Strivers_Sheet/loopDoWhile.java
@@ -0,0 +1,11 @@
+public class loopDoWhile{
+    public static void main(String args[]){
+        int n = 10;
+
+        do{
+            System.out.println("num ="+n);
+            n--;
+        }while(n>=0);
+
+    }
+}
