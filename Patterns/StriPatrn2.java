16e0c7aa37639333ed854779e77b23c775a6ac5d
Aditya Bhole <adityabhole0304@gmail.com>
2025-09-12 14:41:34 +0530

    Strivers pattern Second

---
+++ Patterns/StriPatrn2.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class StriPatrn{
+    public static void Pattern2(int n){
+        for( int i = 0; i < n ; i++){
+            for(int j =0; j<= i ; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String args[]){
+        int n = 5;
+        Pattern2(n);
+    }
+
+}
