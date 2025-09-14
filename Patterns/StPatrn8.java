02f8b2ac954e03760f04aa72e90c8c089973c644
Aditya Bhole <adityabhole0304@gmail.com>
2025-09-14 11:42:05 +0530

    comments

--- Patterns/StPatrn8.java
+++ Patterns/StPatrn8.java
@@ -1,6 +1,7 @@
 public class stPatrn8{
     public static void main(String args[]){
         int n =5;
+        // inverted Triangle pattern
         for(int i = n-1; i>=0; i--){
             for(int j =0; j< n-i; j++ ){
                 System.out.print(" ");
