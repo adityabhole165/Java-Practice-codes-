b5fd0eae71fcd1f0d45c441881390f6457318597
Aditya Bhole <adityabhole0304@gmail.com>
2025-09-14 11:24:02 +0530

    Inverted triangle 1my code

---
+++ Patterns/StPatrn8.java
@@ -0,0 +1,17 @@
+public class stPatrn8{
+    public static void main(String args[]){
+        int n =5;
+        for(int i = n-1; i>=0; i--){
+            for(int j =0; j< n-i; j++ ){
+                System.out.print(" ");
+            }
+            for(int j = 0; j < (2*i)+ 1 ; j++){
+                System.out.print("*");
+            }
+            for(int j =0 ; j< n-i; j++){
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+    }
+}
