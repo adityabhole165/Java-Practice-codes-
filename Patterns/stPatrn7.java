77fa2f60f6ea801f87b0ce9beb799d479bf6c942
Aditya Bhole <adityabhole0304@gmail.com>
2025-09-16 18:55:26 +0530

    Pattern Half Diamound Pattern

--- Patterns/stPatrn7.java
+++ Patterns/stPatrn7.java
@@ -4,7 +4,7 @@ public class stPatrn7{
 
         for(int i = 0; i <n  ; i++){
             for(int j = 0; j < (n -i+1 ); j++){                
-                System.out.print(" ");
+                System.out.print(" ");    
                 
             }
             for(int j = 0; j < ((2*i +1)); j++ ){
