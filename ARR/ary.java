f0b2305099ff110d8184ff0b06060f7a1c18bcd3
Aditya Bhole <adityabhole0304@gmail.com>
2025-10-05 14:35:04 +0530

    Array

---
+++ ARR/ary.java
@@ -0,0 +1,15 @@
+// type arrayname[] = new type[size];
+import java.util.*;
+public class ary{
+    public static void main(String args[]){
+        int mark[] = new int[5];
+        mark[0] = 50 ;
+        mark[1] = 10 ;
+        for( int i = 0; i<mark.length; i++){
+            System.out.println(mark[i]);
+        }
+      
+    }
+}
+
+// type arrname[] = {1,2,34,4} ;
