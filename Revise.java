8e49039c0a2386a236df7b409e1c1cf4366fe855
Aditya Bhole <adityabhole0304@gmail.com>
2025-10-05 14:34:51 +0530

    Revision

---
+++ Revise.java
@@ -0,0 +1,7 @@
+public class Revise{
+    public static void main(String args[]){
+        System.out.print("Hei");
+        System.out.println("Hello world");
+        System.out.print("\n another way to go to next line");
+    }
+}
